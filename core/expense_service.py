b487f007965aee5d49ed60c6e6048665e4c60c56
sollbs <solmybs@hotmail.com>
2026-03-05 13:03:09 +0000

    Aplicar ruff format otra vez

--- core/expense_service.py
+++ core/expense_service.py
@@ -76,7 +76,7 @@ class ExpenseService:
         return sum(expense.amount for expense in self._repository.list_all())
 
     def total_by_month(self) -> dict[str, float]:
-        totals: dict[str,float] = {}
+        totals: dict[str, float] = {}
 
         for expense in self._repository.list_all():
             key = expense.expense_date.strftime("%Y-%m")
